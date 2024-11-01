5a8854bb4c3f33bf31764c59295397eb8a22d761
D3v3shPat3l <devesh14@hotmail.co.uk>
2024-11-01 23:59:17 +0000

    breeze fully working

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
+            output: 'public/build', // Ensure this matches your setup
             refresh: true,
         }),
     ],
