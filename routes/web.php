2a0a786ae621be4c61b805e9a7f7ef5d993606cd
D3v3shPat3l <devesh14@hotmail.co.uk>
2024-11-02 01:54:29 +0000

    reverse back before mess up

--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,12 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
-use App\Http\Controllers\MainPageController;
-
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/main', [PostController::class, 'index'])->name('main.page');
-    Route::post('/posts', [PostController::class, 'create'])->name('posts.create');
-    Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
 });
 
 require __DIR__.'/auth.php';
-
-
-
