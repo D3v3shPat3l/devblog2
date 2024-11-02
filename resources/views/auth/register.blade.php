166d24a6a93a7454ad088126b9978525bfa02cf7
D3v3shPat3l <devesh14@hotmail.co.uk>
2024-11-02 15:39:42 +0000

    title change

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register - ShareIT</title>
+    <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="bg-cover bg-center h-screen flex items-center justify-center" style="background-image: url('/images/background.jpg');">
